206286734f640f8ec81346fbaa36121571d0a8f4
Karsei <karseidev@gmail.com>
2022-07-25 23:37:41 +0900

    initial commit

---
+++ src/main/java/com/example/qaurtz/QaurtzApplication.java
@@ -0,0 +1,13 @@
+package com.example.qaurtz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QaurtzApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QaurtzApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/qaurtz/QaurtzApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.qaurtz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QaurtzApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
